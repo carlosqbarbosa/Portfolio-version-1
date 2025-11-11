cb3d241bb1d66c4efca135bc2c66604f5799c488
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-11-11 14:24:14 -0300

    build: preparando para deploy no GitHub Pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ProjectDetails from "../pages/ProjectDetails";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projeto/:id" element={<ProjectDetails />} />
       </Routes>
-    </BrowserRouter>
   );
 }
