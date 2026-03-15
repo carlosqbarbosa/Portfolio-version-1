41ce533d52cfab71073b5b29b72377ec2da28d84
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2026-03-15 11:41:30 -0300

    atualizando caminho foto perfil

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -191,7 +191,7 @@ export default function Hero() {
               }}
             >
               <img
-                src="https://carlosqbarbosa.github.io/Portfolio-version-1/assets/minhaFoto-C3tFEVZG.jpg"
+                src="src/assets/minhaFoto.jpg"
                 alt="Carlos Barbosa"
                 style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }}
               />
